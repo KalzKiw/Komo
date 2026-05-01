4ed8d870ae53817b32f8c747e8a472f7525e2d36
KalzKiw <alejandrobrito31@gmail.com>
2026-05-01 21:32:20 +0200

    Fix runtime type import

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+/// <reference path="./types/express.d.ts" />
+
 import cors from "cors";
 import express from "express";
 import path from "path";
 import swaggerUi from "swagger-ui-express";
 
-import "./types/express";
 import { swaggerSpec } from "./config";
 import { errorHandler } from "./middlewares/error.middleware";
 import { apiRouter } from "./routes";
